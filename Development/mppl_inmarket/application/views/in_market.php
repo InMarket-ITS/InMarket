9b4e04b64f62bc4ab5aa61007e57a3d182beba3f
santensuru <djuned.ong@gmail.com>
2015-03-01 22:21:28 +0700

    Consumer View
    Change some detail

--- Development/mppl_inmarket/application/views/in_market.php
+++ Development/mppl_inmarket/application/views/in_market.php
@@ -111,8 +111,8 @@
 							<div class="item">
 								<div class="col-sm-6">
 									<h1><span>In</span>Market</h1>
-									<h2>Free Ecommerce Template</h2>
-									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+									<h2>Deterjen Rinso dengan Pewangi Molto</h2>
+									<p>Ingin baju Anda selembut sutra? Gunakan produk ini untuk mencucinya.</p>
 									<button class="btn btn-default get">Tersedia</button>
 								</div>
 								<div class="col-sm-6">
