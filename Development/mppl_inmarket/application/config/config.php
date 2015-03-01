3a4445b2b7311c91764ff87eeeaf88aad5086930
santensuru <djuned.ong@gmail.com>
2015-03-01 16:31:34 +0700

    Old Config

--- Development/mppl_inmarket/application/config/config.php
+++ Development/mppl_inmarket/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://'.$_SERVER["HTTP_HOST"].'/mppl_inmarket';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'mppl';
 
 /*
 |--------------------------------------------------------------------------
