f9c043be26f26f4e559e459e8fe5214bd4357b45
santensuru <djuned.ong@gmail.com>
2015-03-01 16:18:29 +0700

    Consumer View

---
+++ Development/mppl_inmarket/application/controllers/in_market.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class In_market extends CI_Controller {
+	
+	/* default */
+	public function index()	{
+		$this->load->view("in_market");
+	}
+
+	
+}
+
+
+/* End of file in_market.php */
+/* Location: ./application/controllers/in_market.php */
