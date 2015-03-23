4f5145eb12d3ecc8843a4db8b5a5828b253764b5
Muhammad Aunorafiq Musa <raffi.musa07@gmail.com>
2015-03-23 12:50:07 +0700

    navbar

--- Development/mppl_inmarket/application/views/navbar.php
+++ Development/mppl_inmarket/application/views/navbar.php
@@ -13,22 +13,7 @@
     <!-- Top Menu Items -->
     <ul class="nav navbar-right top-nav">
 
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i> <b class="caret"></b></a>
-            <ul class="dropdown-menu alert-dropdown">
-                <li>
-                    <a href="#">Alert Name <span class="label label-default">Alert Badge</span></a>
-                </li>
-                <li>
-                    <a href="#">Alert Name <span class="label label-primary">Alert Badge</span></a>
-                </li>
-
-                <li class="divider"></li>
-                <li>
-                    <a href="#">View All</a>
-                </li>
-            </ul>
-        </li>
+        
         <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Administrator <b class="caret"></b></a>
             <ul class="dropdown-menu">
